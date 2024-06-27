f0fefd797dae43ee6e10895afb052fd90c05ebf5
edysnakee <contactoedysnake@gmail.com>
2024-06-27 12:08:12 -0600

    feat: vistas y templane integrado

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,27 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
+| * Route::get -> Consultar
+| * Route::post -> Guardar
+| * Route::delete -> Eliminar
+| * Route::Put -> Actualizar
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('Home');
+})->name('home');
+
+Route::get('blog', function () {
+    //CONSULTA LISTADO
+    $posts = [
+        ['id' => 1, 'title' => 'php', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'laravel', 'slug' => 'laravel'],
+    ];
+    return view('blog', ['post' => $posts]);
+})->name('blog');
+
+Route::get('blog/{slug}', function ($slug) {
+    //CONSULTA POST
+    $post = $slug;
+    return view('post', ['post' => $post]);
+})->name('post');
